747b52a9522e5151851bfb5637f9a8b55717cfca
NicolePell <pellicena@gmail.com>
2014-11-09 21:43:04 +0000

    post can be added

--- spec/features/posts_feature_spec.rb
+++ spec/features/posts_feature_spec.rb
@@ -8,4 +8,18 @@ describe 'posts' do
       expect(page).to have_link 'Add a post'
     end
   end
+
+  context 'posts have been added' do
+    before do
+      Post.create(name: 'magic')
+  end
+
+
+    it 'should display posts' do
+        visit '/posts'
+        expect(page).to have_content('magic')
+        save_and_open_page
+        expect(page).not_to have_content('No posts')
+    end
+  end
 end
