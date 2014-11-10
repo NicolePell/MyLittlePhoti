5cdd5e38bf21705f1e039f002ccb318750652dad
NicolePell <pellicena@gmail.com>
2014-11-10 23:41:38 +0000

    navbar added and posts centered

---
+++ spec/features/comments_feature_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+describe 'commenting' do
+  before do
+    Post.create(name: 'magic')
+  end
+
+  fit 'allows users to leave a comment using a form' do
+    visit '/posts'
+    click_link 'Comment on magic'
+    fill_in 'Thoughts', with: 'Brohoof!'
+    click_button 'Post comment'
+    click_link 'magic'
+    expect(page).to have_content('Brohoof!')
+  end
+end
